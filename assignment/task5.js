c808ec9d531ecb0f8b6cfbd105ad7e0d8f666069
bhagat-abhishek <inst.abhishek@gmail.com>
2024-04-12 02:17:51 +0530

    AS 5

---
+++ assignment/task5.js
@@ -0,0 +1,14 @@
+//  JSON Object
+
+const weather = {
+  date: Date.now(),
+  temperature: "30 C",
+  conditions: {
+    morninng: "sunny",
+    afternoon: "sunny",
+    evening: "rainfall",
+  },
+  humidity: "mild",
+};
+
+console.log(weather);
